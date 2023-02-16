19cc3e055f8a2e04d6f4c36789211b5ae9f77f7b
RinoCerrone <tkoma999@gmail.com>
2023-02-16 15:01:09 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PIN_projekt_kviz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
